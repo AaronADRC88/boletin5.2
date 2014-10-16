b393aa7e8d44234755d7d822af5c19392d3b4f7c
aferreiradominguez <aferreiradominguez@eniac27.danielcastelao.org>
2014-10-16 12:35:37 +0200

    done

---
+++ src/boletin5/pkg2/Boletin52.java
@@ -0,0 +1,15 @@
+/*satelite*/
+package boletin5.pkg2;
+
+/**
+ *
+ * @author aferreiradominguez
+ */
+public class Boletin52 {
+    
+    public static void main(String[] args) {
+       Satelite sat1=new Satelite();
+       sat1.verPosicion();
+    }
+    
+}
---
+++ src/boletin5/pkg2/Satelite.java
@@ -0,0 +1,29 @@
+package boletin5.pkg2;
+
+/**
+ *
+ * @author aferreiradominguez
+ */
+public class Satelite {
+    private double meridiano ;
+    private double paralelo ;
+    private double distanciaTerra ;
+
+ public Satelite ( ) {
+
+  meridiano = paralelo = distanciaTerra = 0 ;
+}
+
+    public Satelite ( double m, double p , double d ) {
+
+meridiano = m ;
+paralelo = p;
+distanciaTerra = d ;
+
+}
+public void verPosicion (){
+System.out.println("o satelite atopase no paralelo"+paralelo+"meridiano"+meridiano+"a unha distancia da terra"+distanciaTerra);
+
+}
+
+}
